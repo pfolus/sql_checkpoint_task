2f615eadb8a6659c47c9c2273973eff585624d42
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-07-21 21:16:48 +0200

    Ensure tests order.
    Add prefix to all tests names so that they are ordered. By default Unittest starts tests in aplhabetical orded

--- test.py
+++ test.py
@@ -8,26 +8,26 @@ class TestCheckpointExcersise(unittest.TestCase):
 
     # Addreses tests
 
-    def test_address_person(self):
+    def test_step01_address_person(self):
         self.assertEqual("Jan Kowalski", self.get_address().person)
 
-    def test_work_address_person(self):
+    def test_step02_work_address_person(self):
         self.assertEqual("Adam Adamski", self.get_work_address().person)
 
-    def test_company(self):
+    def test_step03_company(self):
         self.assertEqual("Mordor sp. z o.o.", self.get_work_address().company)
 
-    def test_get_full_address(self):
+    def test_step04_get_full_address(self):
         self.assertEqual('Jan Kowalski, Kraków, ul. Daszyńskiego 15/31',
                          self.get_address().get_full_address())
 
-    def test_get_full_work_address(self):
+    def test_step05_get_full_work_address(self):
         self.assertEqual(
                          'Adam Adamski, Warszawa, ul.' +
                          ' Domaniewska 6/66, Mordor sp. z o.o.',
                          self.get_work_address().get_full_address())
 
-    def test_address_eq(self):
+    def test_step06_address_eq(self):
         from address import Address
         first = Address("Jan Kowalski", "Kraków",
                         "ul. Daszyńskiego", "15/31")
@@ -35,7 +35,7 @@ class TestCheckpointExcersise(unittest.TestCase):
                          "ul. Daszyńskiego", "15/31")
         self.assertEqual(first, second)
 
-    def test_address_not_eq(self):
+    def test_step07_address_not_eq(self):
         from address import Address
         first = Address("Jan Kowalski", "Kraków",
                         "ul. Daszyńskiego", "15/31")
@@ -43,7 +43,7 @@ class TestCheckpointExcersise(unittest.TestCase):
                          "ul. Daszyńskiego", "16/31")
         self.assertNotEqual(first, second)
 
-    def test_work_address_eq(self):
+    def test_step08_work_address_eq(self):
         from work_address import WorkAddress
         first = WorkAddress("Adam Adamski", "Warszawa",
                             "ul. Domaniewska", "6/66", "Mordor sp. z o.o.")
@@ -51,7 +51,7 @@ class TestCheckpointExcersise(unittest.TestCase):
                              "ul. Domaniewska", "6/66", "Mordor sp. z o.o.")
         self.assertEqual(first, second)
 
-    def test_work_address_not_eq(self):
+    def test_step09_work_address_not_eq(self):
         from work_address import WorkAddress
         first = WorkAddress("Adam Adamski", "Warszawa",
                             "ul. Domaniewska", "6/66", "Mordor sp. z o.o.")
@@ -59,7 +59,7 @@ class TestCheckpointExcersise(unittest.TestCase):
                              "ul. Domaniewska", "6/66", "Hobbit sp. z o.o.")
         self.assertNotEqual(first, second)
 
-    def test_work_and_address_not_eq(self):
+    def test_step10_work_and_address_not_eq(self):
         from address import Address
         from work_address import WorkAddress
         first = Address("Adam Adamski", "Warszawa",
@@ -70,46 +70,46 @@ class TestCheckpointExcersise(unittest.TestCase):
 
     # AddressBook
 
-    def test_address_book_name(self):
+    def test_step11_address_book_name(self):
         from address_book import AddressBook
         self.my_book = AddressBook("friends")
         self.assertEqual("friends", self.my_book.name)
 
-    def test_address_book_add_address(self):
+    def test_step12_address_book_add_address(self):
         self.add_addresses_to_book()
         self.assertEqual(2, len(self.my_book.addresses))
 
-    def test_address_book_type_error(self):
+    def test_step13_address_book_type_error(self):
         from address_book import AddressBook
         book = AddressBook("Test")
         with self.assertRaises(TypeError, msg="Test dupy się nie powiódł ;("):
             book.add_address("dupa")
 
-    def test_address_book_find_returns_empty(self):
+    def test_step14_address_book_find_returns_empty(self):
         self.add_addresses_to_book()
         self.assertListEqual([], self.my_book.find("XXX"),
                              msg="Should return empty list")
 
-    def test_address_book_find_returns_one(self):
+    def test_step15_address_book_find_returns_one(self):
         self.add_addresses_to_book()
         expected = [self.my_book.addresses[0]]
         actual = self.my_book.find("Kraków")
         self.assertListEqual(expected, actual)
 
-    def test_address_book_find_returns_many(self):
+    def test_step16_address_book_find_returns_many(self):
         self.add_addresses_to_book()
         expected = self.my_book.addresses
         actual = self.my_book.find("ski")
         self.assertListEqual(expected, actual)
 
-    def test_address_book_find_case_insensitive(self):
+    def test_step17_address_book_find_case_insensitive(self):
         self.add_addresses_to_book()
         expected = [self.my_book.addresses[1]]
         actual = self.my_book.find("adam")
         self.assertListEqual(expected, actual,
                              msg="Search should be case insensitive")
 
-    def test_sort(self):
+    def test_step18_sort(self):
         import address_book
         address_book.sorted = None  # remove sorted function
         self.add_addresses_to_book()
@@ -117,7 +117,7 @@ class TestCheckpointExcersise(unittest.TestCase):
         self.my_book.sort()
         self.assertListEqual(expected, self.my_book.addresses)
 
-    def test_sort_2(self):
+    def test_step19_sort_2(self):
         import address_book
         address_book.sorted = None  # remove sorted function
         expected = self.create_address_book()
@@ -126,7 +126,7 @@ class TestCheckpointExcersise(unittest.TestCase):
         expected.sort()
         self.assertListEqual(expected.addresses, actual.addresses)
 
-    def test_builtin_sort(self):
+    def test_step20_builtin_sort(self):
         with open("address_book.py", "r") as f:
             content = f.read()
             self.assertEqual(content.find("addresses.sort"), -1,
@@ -135,26 +135,26 @@ class TestCheckpointExcersise(unittest.TestCase):
 
     # 2nd part tests
 
-    def test_create_from_csv_length(self):
+    def test_step21_create_from_csv_length(self):
         from address_book import AddressBook
         book = AddressBook.create_from_csv("book_name", "addresses.csv")
         self.assertIsInstance(book, AddressBook)
         self.assertEqual(5, len(book.addresses))
 
-    def test_create_from_csv_name(self):
+    def test_step22_create_from_csv_name(self):
         from address_book import AddressBook
         book_name = "book_name"
         book = AddressBook.create_from_csv("book_name", "addresses.csv")
         self.assertEqual("book_name", book.name)
 
-    def test_create_from_csv_addresses(self):
+    def test_step23_create_from_csv_addresses(self):
         from address_book import AddressBook
         expected = self.create_address_book().addresses
         book = AddressBook.create_from_csv("book_name", "addresses.csv")
         actual = book.addresses
         self.assertListEqual(expected, actual)
 
-    def test_save_to_csv(self):
+    def test_step24_save_to_csv(self):
         from address_book import AddressBook
         book_name = "book_name"
         book = AddressBook.create_from_csv("book_name", "addresses.csv")
